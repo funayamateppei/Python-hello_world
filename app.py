ceba58edc5acba612cef4215eab26b17e00f244d
funayamateppei <t.funayama0217@gmail.com>
2023-02-22 17:31:08 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template , request 
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = "Hello World"
+    return name
+
+@app.route('/good')
+def good():
+    name = "Good"
+    return name
+
+if __name__ == "__main__":
+
+    app.run()
